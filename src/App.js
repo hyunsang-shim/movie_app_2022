833792c59f846b0897585516d26e7e7e05b0e5fc
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-10-30 16:28:56 +0900

    03-03 두 번째 리액트 기초개념: JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 
 function App() {
-    return <div> Hello!!! </div>
+    return (
+        <div> 
+            <h1>Hello</h1>
+            <Potato />
+        </div>
+        );
+}
+
+function Potato() {
+    return <h1> I Like Potato</h1>;
 }
 
 export default App;
