403aebe47dd77842c478b90dc506ff2467680da5
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-10-30 23:36:18 +0900

    06-2 영화 데이터 화면에 그리기
    06-1 영화 API 사용해 보기
    06-2 영화 데이터 화면에 그리기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 // React.Component 클래스를 상속받는다.
 class App extends React.Component {
@@ -8,12 +9,21 @@ class App extends React.Component {
 		movies: [],
 	};
 
+	getMovies = async () => {
+		const {
+			data: {
+				data: {movies},
+			},
+		} = await  axios.get('https://yts-proxy.now.sh/list_movies.json');
+		// this.setState({movies});		// ES6 에서는 객체 키와 대입할 변수의 이름이 같다면 코드를 축약할 수 있다.
+										// setState({movies: movies});가 setState({movies}); 가 되는 것.
+		this.setState({movies, isLoading: false});
+	}
 
 	componentDidMount() {
 		// 여기에서 영화 데이터 로딩 
-		setTimeout( () => {
-			this.setState({isLoading: false});
-		}, 6000);
+		this.getMovies();
+		
 	}
 
 
