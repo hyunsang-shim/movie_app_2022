649f81240bffd0b481ebcf51ba13b80347de8e3d
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-11-01 21:17:39 +0900

    08-3 내비게이션 만들어 보기

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { HashRouter, Routes, Route} from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 
 function App() {
 	return (
-		<HashRouter>
+		<HashRouter>					
+			<Navigation />
 			<Routes>
 				<Route path="/" exact={true} element={<Home />} />
 				<Route path="/home" exact={true} element={<Home />} />
